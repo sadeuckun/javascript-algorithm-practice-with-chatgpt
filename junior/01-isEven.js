7ef531382a1be917aab8c66ed196c1bc5d8968e7
Furkan Sade Uckun <20080069@stu.omu.edu.tr>
2025-06-03 06:26:14 +0300

    jr-01-isEven

---
+++ junior/01-isEven.js
@@ -0,0 +1,12 @@
+function isEven(n) {
+    // your code here
+    return n % 2 === 0;
+  }
+  
+  // Test cases
+  console.log(isEven(2));     // true
+  console.log(isEven(7));     // false
+  console.log(isEven(0));     // true
+  console.log(isEven(-4));    // true
+  console.log(isEven(-3));    // false
+  
